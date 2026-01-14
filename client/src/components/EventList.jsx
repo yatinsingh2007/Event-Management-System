ceb12dec2f98960e4643ff1956f384ced4f0f68b
yatinsingh2007 <yatin4591@gmail.com>
2026-01-14 22:31:59 +0530

    button removed

--- client/src/components/EventList.jsx
+++ client/src/components/EventList.jsx
@@ -162,9 +162,6 @@ const EventList = () => {
                     </div>
                 )}
             </div>
-            <button onClick={handleSearch} className='bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition-colors'>
-                Search Events
-            </button>
         </div>
     );
 };
