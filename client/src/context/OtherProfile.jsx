0bc38ed548a9473e0da578d6f2eac06e78114b50
yatinsingh2007 <yatin4591@gmail.com>
2026-01-14 22:30:23 +0530

    useEffect block added

--- client/src/context/OtherProfile.jsx
+++ client/src/context/OtherProfile.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState , useEffect } from "react";
 import React from "react";
 
 const OtherProfileContext = createContext(null);
