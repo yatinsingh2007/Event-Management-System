ded87edf89d13b151653117db6045a367ce62d1b
yatinsingh2007 <yatin4591@gmail.com>
2026-01-14 22:29:45 +0530

    another useEFfect block added

--- client/src/context/OtherProfile.jsx
+++ client/src/context/OtherProfile.jsx
@@ -6,6 +6,9 @@ const OtherProfileContext = createContext(null);
 
 const OtherProfileProvider = ({ children }) => {
     const [selectedUser, setSelectedUser] = useState(JSON.parse(localStorage.getItem("otherSelectedUser")) || []);
+    useEffect(() => {
+        localStorage.setItem("otherSelectedUser", JSON.stringify(selectedUser));
+    } , [selectedUser])
     return (
         <OtherProfileContext.Provider value={{ selectedUser, setSelectedUser }}>
             {children}
